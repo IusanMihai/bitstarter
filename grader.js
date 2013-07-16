3b081c8392b161b23cafdeff4528532e47531b0c
Ubuntu <ubuntu@ip-172-31-36-211.us-west-2.compute.internal>
2013-07-16 09:48:11 +0000

    Updated grader.js to accept url input

--- grader.js
+++ grader.js
@@ -24,51 +24,91 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "none";
+var TEMP_FILE = "temp.html";
 
-var assertFileExists = function(infile) {
+var assertFileExists = function (infile) {
   var instr = infile.toString();
-  if(!fs.existsSync(instr)) {
+  if (!fs.existsSync(instr)) {
     console.log("%s does not exist. Exiting.", instr);
     process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
   }
   return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
+var cheerioHtmlFile = function (htmlfile) {
+  //console.log(htmlfile);
   return cheerio.load(fs.readFileSync(htmlfile));
 };
 
-var loadChecks = function(checksfile) {
+var loadChecks = function (checksfile) {
   return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
+var checkHtmlFile = function (htmlfile, checksfile) {
   $ = cheerioHtmlFile(htmlfile);
   var checks = loadChecks(checksfile).sort();
   var out = {};
-  for(var ii in checks) {
+  for (var ii in checks) {
     var present = $(checks[ii]).length > 0;
     out[checks[ii]] = present;
   }
   return out;
 };
 
-var clone = function(fn) {
+var clone = function (fn) {
   // Workaround for commander.js issue.
   // http://stackoverflow.com/a/6772648
   return fn.bind({});
 };
 
-if(require.main == module) {
+if (require.main == module) {
+  var checkJson;
+
   program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <html_url>', 'Path to index.html url', URL_DEFAULT)
     .parse(process.argv);
-  var checkJson = checkHtmlFile(program.file, program.checks);
-  var outJson = JSON.stringify(checkJson, null, 4);
-  console.log(outJson);
+  if (program.url != "none") {
+    var request_done = false;
+    var file_request;
+    rest.get(program.url).on('complete', function (result) {
+      if (result instanceof Error) {
+        sys.puts('Error: ' + result.message);
+        this.retry(5000); // try again after 5 sec
+      } else {
+        //console.log(result);
+        fs.writeFileSync(TEMP_FILE, result);  //create temporary file
+        file_request = result;
+        //console.log("Script: " + __filename + "\nWrote: " + result + "To: " + TEMP_FILE);
+
+        checkJson = checkHtmlFile(TEMP_FILE, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+        fs.unlink(TEMP_FILE, function (err) {
+          if (err) {
+            console.log(JSON.stringify(err));
+          }
+        });
+      }
+
+    });
+    //fs.writeFileSync(TEMP_FILE, file_request);  //create temporary file
+
+    //fs.unlink(TEMP_FILE); //remove temporary file
+
+  } else {
+    checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+  }
+
+
 } else {
   exports.checkHtmlFile = checkHtmlFile;
 }
+
